2a710870c32d75777a1a349a26e2dbf708f5fe03
victorfdez <vicfernandezespana@gmail.com>
2025-02-19 18:08:01 +0100

    Subiendo nivel 2 con todo implementado menos el cooldown

--- Scenes/Levels/soltar.gd
+++ Scenes/Levels/soltar.gd
@@ -1,11 +1,5 @@
+# Mesa.gd
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var cafe: bool = false
+var lechuga: bool = false
--- Scripts/caracollvl_2.gd
+++ Scripts/caracollvl_2.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 var speed: float = 200
-var cafe: bool = false
+var cafeRecogido: bool = false
+var lechugaRecogida: bool = false
 var mesas: Array = []
 var mesa_seleccionada: Node2D = null
+var pideCafe: bool = false
+var pideLechuga: bool = false
 var segundos: int = 15
 var tiempo_acumulado: float = 0.0
 
@@ -11,12 +14,38 @@ var tiempo_acumulado: float = 0.0
 
 func _ready() -> void:
 	for i in range(1, 7):
+		var nombremesa = ""
+		var cafe = false
+		var lechuga = false
+
 		var mesa = get_node("../Mesa" + str(i))
 		if mesa:
-			# creaomos un objeto mesa con el nombre de la mesa, y 2 variables, cafe y lechuga que seran booleanas de manera random pero una si o si tiene que ser true
+			nombremesa = "Mesa" + str(i)
+			var random = randi() % 2
+			if random == 0:
+				cafe = true
+				random = randi() % 2
+				if random == 0:
+					lechuga = true
+				else:
+					lechuga = false
+			else:
+				cafe = false
+				lechuga = true
 
+			var mesa_incrustada = {
+					"node": mesa,
+					"name": nombremesa,
+					"cafe": cafe,
+					"lechuga": lechuga
+				}
+			mesas.append(mesa_incrustada)
 			
-			mesas.append(mesa)
+	# mostrar mesas
+	for mesa in mesas:
+		print(mesa.name + " - Café: " + str(mesa.cafe) + " - Lechuga: " + str(mesa.lechuga))
+
+	seleccionar_mesa()
 
 func _process(_delta: float) -> void:
 	var direction = Vector2.ZERO
@@ -49,99 +78,145 @@ func rotate_character(direction: Vector2) -> void:
 	elif direction == Vector2.UP:
 		rotation_degrees = 0
 
-func _on_recoger_body_entered(_body: Node2D) -> void:
-	if not cafe:
-		cafe = true
-		seleccionar_mesa()
+func _on_recoger_cafe_body_entered(_body: Node2D) -> void:
+	if not cafeRecogido:
+		cafeRecogido = true
+		print("Café recogido.")
+
+func _on_recoger_lechuga_body_entered(_body: Node2D) -> void:
+	if not lechugaRecogida:
+		lechugaRecogida = true
+		print("Lechuga recogida.")
+
+
+func _on_tirar_cafe_body_entered(_body: Node2D) -> void:
+	if cafeRecogido:
+		cafeRecogido = false
+		print("Café deshechado.")
+
+func _on_tirar_lechuga_body_entered(_body: Node2D) -> void:
+	if lechugaRecogida:
+		lechugaRecogida = false
+		print("Lechuga deshechada.")
 
 func seleccionar_mesa() -> void:
 	if mesas.size() > 0:
 		var indice = randi() % mesas.size()
-		mesa_seleccionada = mesas[indice]
+		mesa_seleccionada = mesas[indice]["node"]
+		pideCafe = mesas[indice]["cafe"]
+		pideLechuga = mesas[indice]["lechuga"]
 		actualizar_cartel()
 
 func actualizar_cartel() -> void:
 	if mesa_seleccionada:
-		print("Dirígete a: " + mesa_seleccionada.name)  # Aquí puedes actualizar un Label en la interfaz
+		print("Dirígete a: " + mesa_seleccionada.name)
+		print("Café: " + str(pideCafe) + " - Lechuga: " + str(pideLechuga))
 
 func actualizar_tiempo(delta: float) -> void:
 	tiempo_acumulado += delta
 	if tiempo_acumulado >= 1.0:
 		tiempo_acumulado -= 1.0
 		segundos = max(0, segundos - 1)
-		print("Tiempo restante: " + str(segundos))
-		if segundos <= 0:
-			print("Tiempo agotado.")
+		# print("Tiempo restante: " + str(segundos))
+		# if segundos <= 0:
+			# print("Tiempo agotado.")
 
 
 func _on_mesa_1_body_entered(_body: Node2D) -> void:
-	if cafe and "Mesa1" == mesa_seleccionada.name:
-		cafe = false
-		mesas.erase(mesa_seleccionada)
-		mesa_seleccionada = null
-		segundos += 10
-		if mesas.size() == 0:
-			print("No quedan más mesas.")
+	if "Mesa1" == mesa_seleccionada.name:
+		if pideCafe == cafeRecogido and pideLechuga == lechugaRecogida:
+			cafeRecogido = false
+			lechugaRecogida = false
+			mesas.erase(mesa_seleccionada)
+			mesa_seleccionada = null
+			segundos += 10
+			if mesas.size() == 0:
+				print("No quedan más mesas.")
+			else:
+				seleccionar_mesa()
 		else:
-			print("Vuelve a por otro café.")
+			print("No has traido la comanda correcta, deshecha lo que no se ha requerido y recoge lo que se te ha pedido.")	
 
 
 
 func _on_mesa_2_body_entered(_body: Node2D) -> void:
-	if cafe and "Mesa2" == mesa_seleccionada.name:
-		cafe = false
-		mesas.erase(mesa_seleccionada)
-		mesa_seleccionada = null
-		segundos += 10
-		if mesas.size() == 0:
-			print("No quedan más mesas.")
+	if "Mesa2" == mesa_seleccionada.name:
+		if pideCafe == cafeRecogido and pideLechuga == lechugaRecogida:
+			cafeRecogido = false
+			lechugaRecogida = false
+			mesas.erase(mesa_seleccionada)
+			mesa_seleccionada = null
+			segundos += 10
+			if mesas.size() == 0:
+				print("No quedan más mesas.")
+			else:
+				seleccionar_mesa()
 		else:
-			print("Vuelve a por otro café.")
+			print("No has traido la comanda correcta, deshecha lo que no se ha requerido y recoge lo que se te ha pedido.")	
 
 
 func _on_mesa_3_body_entered(_body: Node2D) -> void:
-	if cafe and "Mesa3" == mesa_seleccionada.name:
-		cafe = false
-		mesas.erase(mesa_seleccionada)
-		mesa_seleccionada = null
-		segundos += 10
-		if mesas.size() == 0:
-			print("No quedan más mesas.")
+	if "Mesa3" == mesa_seleccionada.name:
+		if pideCafe == cafeRecogido and pideLechuga == lechugaRecogida:
+			cafeRecogido = false
+			lechugaRecogida = false
+			mesas.erase(mesa_seleccionada)
+			mesa_seleccionada = null
+			segundos += 10
+			if mesas.size() == 0:
+				print("No quedan más mesas.")
+			else:
+				seleccionar_mesa()
 		else:
-			print("Vuelve a por otro café.")
+			print("No has traido la comanda correcta, deshecha lo que no se ha requerido y recoge lo que se te ha pedido.")	
 
 
 func _on_mesa_4_body_entered(_body: Node2D) -> void:
-	if cafe and "Mesa4" == mesa_seleccionada.name:
-		cafe = false
-		mesas.erase(mesa_seleccionada)
-		mesa_seleccionada = null
-		segundos += 10
-		if mesas.size() == 0:
-			print("No quedan más mesas.")
+	if "Mesa4" == mesa_seleccionada.name:
+		if pideCafe == cafeRecogido and pideLechuga == lechugaRecogida:
+			cafeRecogido = false
+			lechugaRecogida = false
+			mesas.erase(mesa_seleccionada)
+			mesa_seleccionada = null
+			segundos += 10
+			if mesas.size() == 0:
+				print("No quedan más mesas.")
+			else:
+				seleccionar_mesa()
 		else:
-			print("Vuelve a por otro café.")
+			print("No has traido la comanda correcta, deshecha lo que no se ha requerido y recoge lo que se te ha pedido.")	
 
 func _on_mesa_5_body_entered(_body: Node2D) -> void:
-	if cafe and "Mesa5" == mesa_seleccionada.name:
-		cafe = false
-		mesas.erase(mesa_seleccionada)
-		mesa_seleccionada = null
-		segundos += 10
-		if mesas.size() == 0:
-			print("No quedan más mesas.")
+	if "Mesa5" == mesa_seleccionada.name:
+		if pideCafe == cafeRecogido and pideLechuga == lechugaRecogida:
+			cafeRecogido = false
+			lechugaRecogida = false
+			mesas.erase(mesa_seleccionada)
+			mesa_seleccionada = null
+			segundos += 10
+			if mesas.size() == 0:
+				print("No quedan más mesas.")
+			else:
+				seleccionar_mesa()
 		else:
-			print("Vuelve a por otro café.")
+			print("No has traido la comanda correcta, deshecha lo que no se ha requerido y recoge lo que se te ha pedido.")	
 
 
 func _on_mesa_6_body_entered(_body: Node2D) -> void:
-	if cafe and "Mesa6" == mesa_seleccionada.name:
-		cafe = false
-		mesas.erase(mesa_seleccionada)
-		mesa_seleccionada = null
-		segundos += 10
-		if mesas.size() == 0:
-			print("No quedan más mesas.")
+	if "Mesa6" == mesa_seleccionada.name:
+		if pideCafe == cafeRecogido and pideLechuga == lechugaRecogida:
+			cafeRecogido = false
+			lechugaRecogida = false
+			mesas.erase(mesa_seleccionada)
+			mesa_seleccionada = null
+			segundos += 10
+			if mesas.size() == 0:
+				print("No quedan más mesas.")
+			else:
+				seleccionar_mesa()
 		else:
-			print("Vuelve a por otro café.")
+			print("No has traido la comanda correcta, deshecha lo que no se ha requerido y recoge lo que se te ha pedido.")	
 			
+
+
+
